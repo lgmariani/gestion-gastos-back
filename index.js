39846aafb2e9d5f5f04c741ca35671b3af89e9cc
Lautaro <lgmariani@gmail.com>
2025-03-25 11:49:11 -0300

    add console info

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
+// Mostrar variables de entorno
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_DATABASE:', process.env.DB_DATABASE);
+console.log('PORT:', process.env.PORT);
+
+
 const app = express();
 const port = process.env.PORT || 3005;
 
